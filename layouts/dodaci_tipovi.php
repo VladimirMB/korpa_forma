aa5646d2a7f73bfccea3588a7bae69564d4297e3
VladimirMB <vladimir.m.bogojevic@gmail.com>
2025-08-07 13:56:07 +0200

    Add files via upload

---
+++ layouts/dodaci_tipovi.php
@@ -0,0 +1,120 @@
+<?php
+$dodaci_tip1 = "<div class='more_details' style='display:none;'>
+	<div class='add_to_cart_row'>
+			<select name='number_items' id='number_items'>
+				<option value='1'>1</option>
+				<option value='2'>2</option>
+				<option value='3'>3</option>
+				<option value='4'>4</option>
+				<option value='5'>5</option>
+				<option value='6'>6</option>
+				<option value='7'>7</option>
+				<option value='8'>8</option>
+				<option value='9'>9</option>
+				<option value='10'>10</option>
+			</select>
+			<button class='add_to_cart' type='button'>Dodaj u korpu</button>
+	</div>
+	<div class='order_notice clear'>Napomena: <input type='textarea' rows='5' cols='30' name='ordernotice' class='ordernotice'>Molimo vas da u ovo polje unesete dodatne napomene. Na primer 'sa dosta salate', 'bez luka' i sl. Određeni zahtevi mogu povećati cenu pripremljenog jela.</div>
+</div>
+";
+
+$dodaci_tip2 = "<div class='more_details' style='display:none;'>
+	<div class='add_to_cart_row'>
+			<select name='number_items' id='number_items'>
+			<option value='1'>1</option>
+			<option value='2'>2</option>
+			<option value='3'>3</option>
+			<option value='4'>4</option>
+			<option value='5'>5</option>
+			<option value='6'>6</option>
+			<option value='7'>7</option>
+			<option value='8'>8</option>
+			<option value='9'>9</option>
+			<option value='10'>10</option>
+			</select>
+			<button class='add_to_cart' type='button'>Dodaj u korpu</button>
+	</div>
+	<div class='dodaci_title'>Dodaci:</div>
+	<div class='dodaci_wrap'>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Čili sos' data-dodatakprice='0' data-type='dodatak'  class='dodatak1'><strong>Čili sos</strong> (Gratis)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Kečap' data-dodatakprice='0' data-type='dodatak'  class='dodatak2'><strong>Kečap</strong> (Gratis)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Kupus' data-dodatakprice='0' data-type='dodatak'  class='dodatak3'><strong>Kupus</strong> (Gratis)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Luk crni' data-dodatakprice='0' data-type='dodatak'  class='dodatak4'><strong>Luk crni</strong> (Gratis)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Majonez' data-dodatakprice='0' data-type='dodatak'  class='dodatak5'><strong>Majonez</strong> (Gratis)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Paradajz' data-dodatakprice='0' data-type='dodatak'  class='dodatak6'><strong>Paradajz</strong> (Gratis)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Urnebes salata' data-dodatakprice='0' data-type='dodatak'  class='dodatak7'><strong>Urnebes salata</strong> (Gratis)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Tartar sos' data-dodatakprice='0' data-type='dodatak'  class='dodatak8'><strong>Tartar sos</strong> (Gratis)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Paprika' data-dodatakprice='0' data-type='dodatak'  class='dodatak9'><strong>Paprika </strong> (Gratis)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Zelena salata' data-dodatakprice='0' data-type='dodatak'  class='dodatak10'><strong>Zelena salata</strong> (Gratis)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Ljutenica' data-dodatakprice='0' data-type='dodatak'  class='dodatak11'><strong>Ljutenica</strong> (Gratis)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Senf' data-dodatakprice='0' data-type='dodatak'  class='dodatak12'><strong>Senf</strong> (Gratis)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Tartar sos' data-dodatakprice='0' data-type='dodatak'  class='dodatak13'><strong>Tartar sos</strong> (Gratis)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Pavlaka' data-dodatakprice='0' data-type='dodatak'  class='dodatak14'><strong>Pavlaka</strong> (Gratis)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Kineska mešavina' data-dodatakprice='0' data-type='dodatak'  class='dodatak15'><strong>Kineska mes.</strong> (Gratis)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Tzatziki salata' data-dodatakprice='30' data-type='dodatak'  class='dodatak16'><strong>Tzatziki salata</strong> (30 rsd)</label></div>
+	</div>
+	<div class='order_notice clear'>Napomena: <input type='textarea' rows='5' cols='30' name='ordernotice' class='ordernotice'>Molimo vas da u ovo polje unesete dodatne napomene. Na primer 'sa dosta salate', 'bez luka' i sl. Određeni zahtevi mogu povećati cenu pripremljenog jela.</div>
+</div>
+";
+
+$dodaci_tip3 = "<div class='more_details' style='display:none;'>
+	<div class='add_to_cart_row'>
+			<select name='number_items' id='number_items'>
+			<option value='1'>1</option>
+			<option value='2'>2</option>
+			<option value='3'>3</option>
+			<option value='4'>4</option>
+			<option value='5'>5</option>
+			<option value='6'>6</option>
+			<option value='7'>7</option>
+			<option value='8'>8</option>
+			<option value='9'>9</option>
+			<option value='10'>10</option>
+			</select>
+			<button class='add_to_cart' type='button'>Dodaj u korpu</button>
+	</div>
+	<div class='dodaci_title'>Dodaci:</div>
+	<div class='dodaci_wrap'>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Pavlaka' data-dodatakprice='0' data-type='dodatak'  class='dodatak1'><strong>Pavlaka</strong> (Gratis)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Tartar sos' data-dodatakprice='0' data-type='dodatak'  class='dodatak2'><strong>Tartar sos</strong> (Gratis)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Senf' data-dodatakprice='0' data-type='dodatak'  class='dodatak3'><strong>Senf</strong> (Gratis)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Tzatziki salata' data-dodatakprice='30' data-type='dodatak'  class='dodatak4'><strong>Tzatziki salata</strong> (30 rsd)</label></div>
+	</div>
+	<div class='order_notice clear'>Napomena: <input type='textarea' rows='5' cols='30' name='ordernotice' class='ordernotice'>Molimo vas da u ovo polje unesete dodatne napomene. Na primer 'sa dosta salate', 'bez luka' i sl. Određeni zahtevi mogu povećati cenu pripremljenog jela.</div>
+</div>
+";
+
+$dodaci_tip4 = "<div class='more_details' style='display:none;'>
+	<div class='add_to_cart_row'>
+			<select name='number_items' id='number_items'>
+			<option value='1'>1</option>
+			<option value='2'>2</option>
+			<option value='3'>3</option>
+			<option value='4'>4</option>
+			<option value='5'>5</option>
+			<option value='6'>6</option>
+			<option value='7'>7</option>
+			<option value='8'>8</option>
+			<option value='9'>9</option>
+			<option value='10'>10</option>
+			</select>
+			<button class='add_to_cart' type='button'>Dodaj u korpu</button>
+	</div>
+	<div class='dodaci_title'>Dodaci:</div>
+	<div class='dodaci_wrap'>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Plazma' data-dodatakprice='0' data-type='dodatak'  class='dodatak1'><strong>Plazma</strong> (Gratis)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Borovnica' data-dodatakprice='100' data-type='dodatak'  class='dodatak2'><strong>Borovnica</strong> (100 rsd)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Ananas' data-dodatakprice='30' data-type='dodatak'  class='dodatak3'><strong>Ananas</strong> (30 rsd)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Banane' data-dodatakprice='50' data-type='dodatak'  class='dodatak4'><strong>Banane</strong> (50 rsd)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Čoko kuglice' data-dodatakprice='50' data-type='dodatak'  class='dodatak5'><strong>Čoko kuglice</strong> (50 rsd)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Čoko bananice' data-dodatakprice='30' data-type='dodatak'  class='dodatak6'><strong>Čoko bananice</strong> (30 rsd)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Čoko mrvice' data-dodatakprice='30' data-type='dodatak'  class='dodatak7'><strong>Čoko mrvice</strong> (30 rsd)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Kikiriki' data-dodatakprice='30' data-type='dodatak'  class='dodatak8'><strong>Kikiriki</strong> (30 rsd)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Kivi' data-dodatakprice='70' data-type='dodatak'  class='dodatak9'><strong>Kivi</strong> (70 rsd)</label></div>
+		<div class='dodaci'><label><input type='checkbox' data-dodatakname='Kokos' data-dodatakprice='80' data-type='dodatak'  class='dodatak10'><strong>Kokos</strong> (80 rsd)</label></div>
+	</div>
+	<div class='order_notice clear'>Napomena: <input type='textarea' rows='5' cols='30' name='ordernotice' class='ordernotice'>Molimo vas da u ovo polje unesete dodatne napomene. Na primer 'sa dosta salate', 'bez luka' i sl. Određeni zahtevi mogu povećati cenu pripremljenog jela.</div>
+</div>
+";
+?>
---
+++ layouts/products.php
@@ -0,0 +1,213 @@
+<?php
+$categories = array( 
+		"1" => array (
+			"category_id" => 1,
+			"category_name" => 'Roštilj',
+			"category_id_name" => 'rostilj'
+		),
+		"2" => array (
+			"category_id" => 2,
+			"category_name" => 'Sendviči',
+			"category_id_name" => 'sendvici'
+		),
+		"3" => array (
+			"category_id" => 3,
+			"category_name" => 'Palačinke',
+			"category_id_name" => 'palacinke'
+		),
+		"4" => array (
+			"category_id" => 4,
+			"category_name" => 'Pića',
+			"category_id_name" => 'pica'
+		),
+		"5" => array (
+			"category_id" => 5,
+			"category_name" => 'Sladoled',
+			"category_id_name" => 'sladoled'
+		),
+	);
+
+$products = array( 
+		"1" => array (
+			"product_id" => 1,
+			"category_id" => 1,
+			"product_name" => 'Giros',
+			"product_price" => 290,
+			"product_desc" => 'Giros opis',
+			"dodatak_type" => 2,
+			//"product_image" => 'link'
+		),
+		"2" => array (
+			"product_id" => 2,
+			"category_id" => 1,
+			"product_name" => 'Punjeni šiš',
+			"product_price" => 220,
+			"product_desc" => 'Punjeni šiš opis',
+			"dodatak_type" => 2,
+			//"product_image" => 'link'	    
+		),
+		"3" => array (
+			"product_id" => 3,
+			"category_id" => 1,
+			"product_name" => 'Rolovani šiš',
+			"product_price" => 220,
+			"product_desc" => 'Rolovani šiš opis',
+			"dodatak_type" => 2,
+			//"product_image" => 'link'	    
+		),
+		"4" => array (
+			"product_id" => 4,
+			"category_id" => 1,
+			"product_name" => 'Pljeskavica',
+			"product_price" => 170,
+			"product_desc" => 'Pljeskavica opis',
+			"dodatak_type" => 2,
+			//"product_image" => 'link'    
+		),
+		"5" => array (
+			"product_id" => 5,
+			"category_id" => 1,
+			"product_name" => 'Punjena pljeskavica',
+			"product_price" => 270,
+			"product_desc" => 'Punjena pljeskavica opis',
+			"dodatak_type" => 2,
+			//"product_image" => 'link'	    
+		),
+		"6" => array (
+			"product_id" => 6,
+			"category_id" => 1,
+			"product_name" => 'Gurmanska pljeskavica',
+			"product_price" => 300,
+			"product_desc" => 'Gurmanska pljeskavica opis',
+			"dodatak_type" => 2,
+			//"product_image" => 'link'	    
+		),
+		"7" => array (
+			"product_id" => 7,
+			"category_id" => 1,
+			"product_name" => 'Karađorđeva šnicla',
+			"product_price" => 400,
+			"product_desc" => 'Karađorđeva šnicla opis',
+			"dodatak_type" => 2,
+			//"product_image" => 'link'	    
+		),
+		"8" => array (
+			"product_id" => 8,
+			"category_id" => 1,
+			"product_name" => 'Pomfrit',
+			"product_price" => 95,
+			"product_desc" => 'Pomfrit opis',
+			"dodatak_type" => 1,
+			//"product_image" => 'link'	    
+		),
+		"9" => array (
+			"product_id" => 9,
+			"category_id" => 2,
+			"product_name" => 'Sendvič Pečenica',
+			"product_price" => 220,
+			"product_desc" => 'Sendvič Pečenica opis',
+			"dodatak_type" => 2,
+			//"product_image" => 'link'	    
+		),
+		"10" => array (
+			"product_id" => 10,
+			"category_id" => 2,
+			"product_name" => 'Sendvič Šunka',
+			"product_price" => 200,
+			"product_desc" => 'Sendvič Šunka opis',
+			"dodatak_type" => 4,
+			//"product_image" => 'link'	    
+		),
+		"11" => array (
+			"product_id" => 11,
+			"category_id" => 3,
+			"product_name" => 'Palačinka Eurokrem',
+			"product_price" => 150,
+			"product_desc" => 'Palačinka Eurokrem opis',
+			"dodatak_type" => 4,
+			//"product_image" => 'link'	    
+		),
+		"12" => array (
+			"product_id" => 12,
+			"category_id" => 3,
+			"product_name" => 'Palačinka Nutella',
+			"product_price" => 170,
+			"product_desc" => 'Palačinka Nutella opis',
+			"dodatak_type" => 4,
+			//"product_image" => 'link'	    
+		),
+		"13" => array (
+			"product_id" => 13,
+			"category_id" => 3,
+			"product_name" => 'Palačinke Šunka',
+			"product_price" => 200,
+			"product_desc" => 'Palačinke Šunka opis',
+			"dodatak_type" => 2,
+			//"product_image" => 'link'	    
+		),
+		"14" => array (
+			"product_id" => 14,
+			"category_id" => 3,
+			"product_name" => 'Palačinke Pečenica',
+			"product_price" => 220,
+			"product_desc" => 'Palačinke Pečenica opis',
+			"dodatak_type" => 3,
+			//"product_image" => 'link'	    
+		),
+		"15" => array (
+			"product_id" => 15,
+			"category_id" => 3,
+			"product_name" => 'Palačinke Pršuta',
+			"product_price" => 250,
+			"product_desc" => 'Palačinke Pršuta opis',
+			"dodatak_type" => 4,
+			//"product_image" => 'link'	    
+		),
+		"16" => array (
+			"product_id" => 16,
+			"category_id" => 3,
+			"product_name" => 'Palačinke Dimljena piletina',
+			"product_price" => 270,
+			"product_desc" => 'Palačinke Dimljena piletina opis',
+			"dodatak_type" => 4,
+			//"product_image" => 'link'	    
+		),
+		"17" => array (
+			"product_id" => 17,
+			"category_id" => 4,
+			"product_name" => 'Coca-Cola 0.3',
+			"product_price" => 70,
+			"product_desc" => 'Coca-Cola 0.3 opis',
+			"dodatak_type" => 1,
+			//"product_image" => 'link'	    
+		),
+		"18" => array (
+			"product_id" => 18,
+			"category_id" => 4,
+			"product_name" => 'Schweppes bitter lemon 0.3',
+			"product_price" => 70,
+			"product_desc" => 'Schweppes bitter lemon 0.3 opis',
+			"dodatak_type" => 1,
+			//"product_image" => 'link'	    
+		),
+		"19" => array (
+			"product_id" => 19,
+			"category_id" => 4,
+			"product_name" => 'Fanta 0.3',
+			"product_price" => 70,
+			"product_desc" => 'Fanta opis',
+			"dodatak_type" => 1,
+			//"product_image" => 'link'	    
+		),
+		"20" => array (
+			"product_id" => 20,
+			"category_id" => 5,
+			"product_name" => 'Sladoled 1',
+			"product_price" => 210,
+			"product_desc" => 'Sladoled 1 opis',
+			"dodatak_type" => 4,
+			//"product_image" => 'link'	    
+		),
+	);
+
+?>
